b8c85609958d009d5cbc6e0b8304614d5a157091
Plotnik <plotnik1992@gmail.com>
2018-11-09 17:01:46 +0300

    Поддержка js

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp 					= require('gulp'),
 		sass 					= require('gulp-sass'),
 		pug 					= require('gulp-pug'),
-		browserSync 	= require('browser-sync');
+		browserSync 	= require('browser-sync'),
+		concat				= require('gulp-concat'),
+		uglify				= require('gulp-uglifyjs');
 		
 
 gulp.task('sass', function () {
@@ -20,6 +22,17 @@ gulp.task('pug', function () {
 	.pipe(browserSync.reload({stream: true}))
 });	
 
+gulp.task('js', function() {
+	return gulp.src([
+		'app/libs/jquery/dist/jquery.min.js',
+		'app/js/common.js',
+	])
+	.pipe(concat('scripts.min.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('app/'))
+	.pipe(browserSync.reload({stream: true}));
+});
+
 gulp.task('browser-sync', function() {
 	browserSync({
 		server: {
@@ -29,10 +42,10 @@ gulp.task('browser-sync', function() {
 	});
 });
 
-gulp.task('watch', ['browser-sync','sass', 'pug'], function() {
+gulp.task('watch', ['sass', 'pug', 'browser-sync'], function() {
 	gulp.watch('app/sass/**/*.sass', ['sass']);
 	gulp.watch('app/pug/**/*.pug', ['pug']);
-	// gulp.watch('app/pug/**/*.pug', ['pug']);
+	gulp.watch('app/js/**/*.js', ['js']);
 });
 
 // gulp.task('default', function() {
