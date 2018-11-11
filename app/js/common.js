ebedacba95b05b0bd2433776383a8a688674adb4
Plotnik <plotnik1992@gmail.com>
2018-11-11 21:47:07 +0300

    Добавил меню с моего codepen

--- app/js/common.js
+++ app/js/common.js
@@ -12,9 +12,29 @@ $(document).ready(function() {
 		}
 	);
 
-		
-		
+	$(".toggle_mnu").toggleClass("not-active");	
 
+	$(".toggle_mnu").click(function() {
+		$(this).toggleClass("active");
+	});
 
-});
+	$(".top_mnu ul a").click(function() {
+		$(".top_mnu").fadeOut(600);
+		$(".sandwich").toggleClass("active");
+		$(".top_text").css("opacity", "1");
+	});
+
+	$(".toggle_mnu").click(function() {
+		if ($(".top_mnu").is(":visible")) {
+			// $(".top_text").css("opacity", "1");
+			$(".top_mnu").fadeOut(600);
+			$(".top_mnu li a").removeClass("fadeInUp animated");
+		} else {
+			$(".top_text").css("opacity", ".1");
+			$(".top_mnu").fadeIn(600);
+			$(".top_mnu li a").addClass("fadeInUp animated");
+		};
+	});
+});  	
+		
 
