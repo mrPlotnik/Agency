785934b292a377dbf42f88afce8ca41173ffe6ba
Plotnik <plotnik1992@gmail.com>
2018-11-11 15:03:38 +0300

    First script. Animation

--- app/js/common.js
+++ app/js/common.js
@@ -1,3 +1,20 @@
-$(function() {
+$(document).ready(function() {
+
+	$(".right-col ul li img").hide();
+
+	$(".right-col ul li").hover(
+		function() {
+			$(this).find("img").removeClass("fadeOutLeft").show().addClass("fadeInLeft").css("animation-delay", ".2s");			
+		},
+		function() {
+			$(this).find("img").removeClass("fadeInLeft").addClass("fadeOutLeft").css("animation-delay", ".2s");
+			
+		}
+	);
+
+		
+		
+
 
 });
+
