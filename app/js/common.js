42d8b9cf27a3d2673a943cfe81964aa3a5077d0d
Plotnik <plotnik1992@gmail.com>
2018-11-15 02:14:43 +0300

    Анимация меню, доработка

--- app/js/common.js
+++ app/js/common.js
@@ -4,10 +4,10 @@ $(document).ready(function() {
 
 	$(".right-col ul li").hover(
 		function() {
-			$(this).find("img").removeClass("fadeOutLeft").show().addClass("fadeInLeft").css("animation-delay", ".2s");			
+			$(this).find("img").removeClass("fadeOutLeft").show().addClass("fadeInLeft").css("animation-delay", ".1s");			
 		},
 		function() {
-			$(this).find("img").removeClass("fadeInLeft").addClass("fadeOutLeft").css("animation-delay", ".2s");
+			$(this).find("img").removeClass("fadeInLeft").addClass("fadeOutLeft").css("animation-delay", ".1s");
 			
 		}
 	);
@@ -18,21 +18,20 @@ $(document).ready(function() {
 		$(this).toggleClass("active");
 	});
 
-	$(".top_mnu ul a").click(function() {
-		$(".top_mnu").fadeOut(600);
-		$(".sandwich").toggleClass("active");
-		$(".top_text").css("opacity", "1");
+	$(".top_m a").click(function() {
+		$(".top_mnu").fadeOut(600);		
 	});
 
 	$(".toggle_mnu").click(function() {
-		if ($(".top_mnu").is(":visible")) {
-			// $(".top_text").css("opacity", "1");
-			$(".top_mnu").fadeOut(600);
-			$(".top_mnu li a").removeClass("fadeInUp animated");
-		} else {
-			$(".top_text").css("opacity", ".1");
+		if ($(".top_mnu").is(":visible")) {			
+			$(".top_mnu").fadeOut(600);			
+			$(".r-mid div").removeClass("fadeIn animated");
+			$(".top_mnu li a").removeClass("fadeInRight animated");
+		} else {			
 			$(".top_mnu").fadeIn(600);
-			$(".top_mnu li a").addClass("fadeInUp animated");
+			$(".r-mid div").addClass("fadeIn animated");
+			$(".top_mnu li a").addClass("fadeInRight animated");
+			
 		};
 	});
 });  	
