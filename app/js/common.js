a1216cd5793922b5700b017d32b2eb6fc225a8a9
Plotnik <plotnik1992@gmail.com>
2018-11-20 00:00:12 +0300

    Доработка старых и добавление новых страниц

--- app/js/common.js
+++ app/js/common.js
@@ -1,23 +1,27 @@
 $(document).ready(function() {
 
 	
+	function pro() {
+		$(".right-col ul li img").hide();
 
-	$(".right-col ul li img").hide();
+		$(".right-col ul li").hover(
+			function() {
+				$(this).find("img").removeClass("fadeOutLeft").show().addClass("fadeInLeft").css("animation-delay", ".1s");	
+			},
+			function() {
+				$(this).find("img").removeClass("fadeInLeft").addClass("fadeOutLeft").css("animation-delay", ".1s");			
+			}
+		);
+	};
+	
 
-	$(".right-col ul li").hover(
-		function() {
-			$(this).find("img").removeClass("fadeOutLeft").show().addClass("fadeInLeft").css("animation-delay", ".1s");			
-		},
-		function() {
-			$(this).find("img").removeClass("fadeInLeft").addClass("fadeOutLeft").css("animation-delay", ".1s");
-			
-		}
-	);
+
+
 
 	$(".toggle_mnu").toggleClass("not-active");	
 
 	$(".toggle_mnu").click(function() {
-		$(this).toggleClass("active");
+		$(this).toggleClass("active");		
 	});
 
 	$(".top_m a").click(function() {
@@ -45,37 +49,59 @@ $(document).ready(function() {
 	heightDetect();
 
 
-	// УРОВНИ САЙТА
-	$(".str-social").hide();
-	$(".str-contact").hide();
+	// УРОВНИ САЙТА	
 
-	$("#references").click(function() {
-		if ($(".str-social").is(":visible")) {
-			$(".str-contact").hide();
-			$(".str-social").hide();
-			$(".str-references").fadeIn(900);
-			$(".left-col").hide().removeClass("left-col-social")
-			$(".left-col").fadeIn(900).addClass("left-col-references");
+	$(document).ready(function() {
 		
-		};	
+		function show() {
+			$.ajax({
+				url: "../main.html",
+				cache: false,
+				success: function(html) {
+					$(".content").html(html);
+				}
+			});
+		}
+		show();
+		
+	
 	});
 
 	$("#social").click(function() {
-		if ($(".str-references").is(":visible")) {
-			$(".str-references").hide();
-			$(".str-contact").hide();
-			$(".str-social").fadeIn(900);
-			$(".left-col").hide().removeClass("left-col-references")
-			$(".left-col").fadeIn(900).addClass("left-col-social");
+		
+		function show() {
+			$.ajax({
+				url: "../social.html",
+				cache: false,
+				success: function(html) {
+					$(".content").html(html);
+				}
+			});
+		}
+		show();
+		
+	
+	});
+
+	$("#offer").click(function() {
+		
+		function show() {
+			$.ajax({
+				url: "../offer.html",
+				cache: false,
+				success: function(html) {
+					$(".content").html(html);
+				}
+			}).done(function() { // Если успешно
+				pro();
+			});
+		}
+		show();
+
 			
-		};	
 	});	
 
-	$("#contact").click(function() {
-		$(".str-references").hide();
-		$(".str-social").hide();
-		$(".str-contact").fadeIn(900);
-	});
+	
 
 });  	
 		
